7433010c78455515545f49b836f4eaff7e98a93a
Mike7704 <Michael.cowley2001@gmail.com>
2025-12-23 23:38:51 +0000

    Added a day and night cycle.

--- src/environment/Lighting.java
+++ src/environment/Lighting.java
@@ -12,6 +12,16 @@ public class Lighting {
 	private GamePanel gamePanel;
 	private RadialGradient darknessGradient;
 	
+	private int dayCounter;
+	float filterAlpha = 0f;
+	
+	// Day states
+	private final int day = 0;
+	private final int dusk = 1;
+	private final int night = 2;
+	private final int dawn = 3;
+	private int dayState = day;
+	
 	public Lighting(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 		setLightSource();
@@ -26,8 +36,8 @@ public class Lighting {
 		        0,
 		        false,
 		        CycleMethod.NO_CYCLE,
-		        new Stop(0.0, Color.rgb(0, 0, 0, 0.98)),
-		        new Stop(1.0, Color.rgb(0, 0, 0, 0.98))
+		        new Stop(0.0, Color.rgb(0, 0, 0, 0.96)),
+		        new Stop(1.0, Color.rgb(0, 0, 0, 0.96))
 		    );
 		}
 		else {
@@ -48,21 +58,74 @@ public class Lighting {
 		        new Stop(0.65, Color.rgb(0, 0, 0, 0.4)),
 		        new Stop(0.8, Color.rgb(0, 0, 0, 0.65)),
 		        new Stop(0.9, Color.rgb(0, 0, 0, 0.85)),
-		        new Stop(1.0, Color.rgb(0, 0, 0, 0.98))		// Dark edges
+		        new Stop(1.0, Color.rgb(0, 0, 0, 0.96))		// Dark edges
 		    );
 		}
 	}
 	
 	public void draw(GraphicsContext gc) {
-	    // Fill the entire screen with darkness
-	    gc.setFill(darknessGradient);
-	    gc.fillRect(0, 0, gamePanel.screenWidth, gamePanel.screenHeight);
+		
+	    if (dayState == night) {
+	    	// Fill the entire screen with darkness and a light source
+		    gc.setFill(darknessGradient);
+		    gc.fillRect(0, 0, gamePanel.screenWidth, gamePanel.screenHeight);
+	    }
+	    else {
+	    	// Apply day-night filter
+	    	gc.setFill(Color.rgb(0, 0, 0, filterAlpha));
+	    	gc.fillRect(0, 0, gamePanel.screenWidth, gamePanel.screenHeight);
+	    }
+	    	    
+	    // Debug
+	    String dayStateString = "";
+	    switch (dayState) {
+		    case day: dayStateString = "Day"; break;
+		    case dusk: dayStateString = "Dusk"; break;
+		    case night: dayStateString = "Night"; break;
+		    case dawn: dayStateString = "Dawn"; break;
+	    }
+	    gc.setFill(Color.WHITE);
+	    gc.fillText("Day State: " + dayStateString, gamePanel.screenWidth - 155, gamePanel.screenHeight - 20);
 	}
 	
 	public void update() {
 		if (gamePanel.getPlayer().lightUpdated) {
 			setLightSource();
 			gamePanel.getPlayer().lightUpdated = false;
+		}
+		
+		// Check the state of the day-night cycle
+		if (dayState == day) {
+			dayCounter++;
+			
+			if (dayCounter > 1200) {
+				dayState = dusk;
+				dayCounter = 0;
+			}
+		}
+		if (dayState == dusk) {
+			filterAlpha += 0.001f;
+			
+			if (filterAlpha > 0.960f) {
+				filterAlpha = 0.960f;
+				dayState = night;
+			}
+		}
+		if (dayState == night) {
+			dayCounter++;
+			
+			if (dayCounter > 600) {
+				dayState = dawn;
+				dayCounter = 0;
+			}
+		}
+		if (dayState == dawn) {
+			filterAlpha -= 0.001f;
+			
+			if (filterAlpha < 0f) {
+				filterAlpha = 0f;
+				dayState = day;
+			}
 		}
 	}
 	
